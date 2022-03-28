563b7e42bcf22529e4b3a40e361d711ed7f92f82
nnnnadia <tristaonadia@gmail.com>
2022-03-28 19:16:58 -0300

    Requisito 8 - Implementados os testes da função fetchProducts.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('É uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Chama a função fetch corretamente', async () => {
+    expect.assertions(2);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('Retorna a busca correta quanto é chamada com \'computador\'', async () => {
+    expect.assertions(1);
+    await expect(fetchProducts('computador')).resolves.toEqual(computadorSearch);
+  });
+  it('Retorna uma mensagem de erro \'You must provide an url\' quando chamada vazia', () => {
+    expect.assertions(1);
+    await expect(fetchProducts()).resolves.toEqual(new Error('You must provide an url'));
+  });
 });
