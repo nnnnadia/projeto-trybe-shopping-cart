4470e140ce3f3ca6360e470c95aecce9f9524c02
nnnnadia <tristaonadia@gmail.com>
2022-03-29 18:04:42 -0300

    Requisito 4 - Corrige escopo da função que carrega lista salva.

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,5 +1,13 @@
 const getSavedCartItems = () => {
-  // seu código aqui
+  const savedCartItems = localStorage.getItem('cartItems');
+  if (savedCartItems === null || savedCartItems === '') {
+    localStorage.setItem('cartItems', []);
+    return;
+  }
+  getCartItems().innerHTML = savedCartItems;
+  getEachCartItem().forEach((item) => {
+    item.addEventListener('click', cartItemClickListener);
+  });
 };
 
 if (typeof module !== 'undefined') {
